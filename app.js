d1cb607dd50c1d059a4922fe9225e30f68056108
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-13 16:23:22 +0700

    seeding sudah selesai

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const bodyparser = require('body-parser');
 const pg = require('pg');
 const Sequelize = require('sequelize')
+const index = require('./router/index.js')
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res)=>{
-    res.render('index');
-  });
+app.use('/',index)
 
 app.listen(3000 || 3000,()=>{
     console.log(`Hello I'm on 3000`);
---
+++ seeders/20170913090825-foodlist.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('FoodLists', [{
+      food: 'Ayam Goreng',
+      amount: 30,
+      price: 15000,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      food: 'Bebek Goreng',
+      amount: 20,
+      price: 17000,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+        food: 'Tahu Goreng',
+        amount: 50,
+        price: 2000,
+        createdAt: new Date(),
+        updatedAt: new Date()  
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
